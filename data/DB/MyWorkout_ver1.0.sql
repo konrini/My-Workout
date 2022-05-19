978f4d0b954838094b1d1cf060b2a41af4861e7a
soomi1015 <99255195+soomi1015@users.noreply.github.com>
2022-05-19 15:18:04 +0900

    Update MyWorkout_ver1.0.sql
    Changed Project name
    My Fitness -> My Workout

--- data/DB/MyWorkout_ver1.0.sql
+++ data/DB/MyWorkout_ver1.0.sql
@@ -5,19 +5,19 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema My_Fitness
+-- Schema My_Workout
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema My_Fitness
+-- Schema My_Workout
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `My_Fitness` DEFAULT CHARACTER SET utf8 ;
-USE `My_Fitness` ;
+CREATE SCHEMA IF NOT EXISTS `My_Workout` DEFAULT CHARACTER SET utf8 ;
+USE `My_Workout` ;
 
 -- -----------------------------------------------------
--- Table `My_Fitness`.`user`
+-- Table `My_Workout`.`user`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `My_Fitness`.`user` (
+CREATE TABLE IF NOT EXISTS `My_Workout`.`user` (
   `user_id` VARCHAR(20) NOT NULL,
   `password` VARCHAR(20) NOT NULL,
   `nickname` VARCHAR(8) NOT NULL,
@@ -31,9 +31,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `My_Fitness`.`video`
+-- Table `My_Workout`.`video`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `My_Fitness`.`video` (
+CREATE TABLE IF NOT EXISTS `My_Workout`.`video` (
   `video_id` INT NOT NULL AUTO_INCREMENT,
   `url` VARCHAR(100) NOT NULL,
   `like_count` INT NOT NULL,
@@ -44,9 +44,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `My_Fitness`.`video_like`
+-- Table `My_Workout`.`video_like`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `My_Fitness`.`video_like` (
+CREATE TABLE IF NOT EXISTS `My_Workout`.`video_like` (
   `video_like_id` INT NOT NULL AUTO_INCREMENT,
   `user_id` VARCHAR(20) NOT NULL,
   `video_id` INT NOT NULL,
@@ -55,21 +55,21 @@ CREATE TABLE IF NOT EXISTS `My_Fitness`.`video_like` (
   INDEX `fk_video_like_video1_idx` (`video_id` ASC) VISIBLE,
   CONSTRAINT `fk_video_like_user`
     FOREIGN KEY (`user_id`)
-    REFERENCES `My_Fitness`.`user` (`user_id`)
+    REFERENCES `My_Workout`.`user` (`user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_video_like_video1`
     FOREIGN KEY (`video_id`)
-    REFERENCES `My_Fitness`.`video` (`video_id`)
+    REFERENCES `My_Workout`.`video` (`video_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `My_Fitness`.`review`
+-- Table `My_Workout`.`review`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `My_Fitness`.`review` (
+CREATE TABLE IF NOT EXISTS `My_Workout`.`review` (
   `review_id` INT NOT NULL AUTO_INCREMENT,
   `content` VARCHAR(100) NOT NULL,
   `time` DATETIME NOT NULL,
@@ -83,21 +83,21 @@ CREATE TABLE IF NOT EXISTS `My_Fitness`.`review` (
   INDEX `fk_review_video1_idx` (`video_id` ASC) VISIBLE,
   CONSTRAINT `fk_review_user1`
     FOREIGN KEY (`user_id`)
-    REFERENCES `My_Fitness`.`user` (`user_id`)
+    REFERENCES `My_Workout`.`user` (`user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_review_video1`
     FOREIGN KEY (`video_id`)
-    REFERENCES `My_Fitness`.`video` (`video_id`)
+    REFERENCES `My_Workout`.`video` (`video_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `My_Fitness`.`friend`
+-- Table `My_Workout`.`friend`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `My_Fitness`.`friend` (
+CREATE TABLE IF NOT EXISTS `My_Workout`.`friend` (
   `friend_id` INT NOT NULL AUTO_INCREMENT,
   `target_id` VARCHAR(20) NOT NULL,
   `follower_id` VARCHAR(20) NOT NULL,
@@ -106,9 +106,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `My_Fitness`.`daily_workout`
+-- Table `My_Workout`.`daily_workout`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `My_Fitness`.`daily_workout` (
+CREATE TABLE IF NOT EXISTS `My_Workout`.`daily_workout` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `category` VARCHAR(3) NOT NULL,
   `diary` VARCHAR(1000) NULL,
@@ -120,7 +120,7 @@ CREATE TABLE IF NOT EXISTS `My_Fitness`.`daily_workout` (
   INDEX `fk_daily_workout_user1_idx` (`user_id` ASC) VISIBLE,
   CONSTRAINT `fk_daily_workout_user1`
     FOREIGN KEY (`user_id`)
-    REFERENCES `My_Fitness`.`user` (`user_id`)
+    REFERENCES `My_Workout`.`user` (`user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
