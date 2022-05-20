585bde278766cdc2693d222555ebf546951e2e08
soomi1015 <99255195+soomi1015@users.noreply.github.com>
2022-05-20 13:59:01 +0900

    upload myworkout-view
    upload myworkout-view Setting Version

---
+++ myworkout-view/src/components/common/HeaderNav.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+      <nav class="header-nav">
+          <div>
+              <router-link>Main</router-link>
+          </div>
+          <div>
+              <router-link>Login</router-link>
+              <router-link>MyPage</router-link>
+          </div>
+      </nav>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
