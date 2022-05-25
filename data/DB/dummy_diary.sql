32bd15f0614c2c8489ad209b2875499da22c387e
Migon <79758079+konrini@users.noreply.github.com>
2022-05-26 01:38:21 +0900

    Update dummy_diary.sql

--- data/DB/dummy_diary.sql
+++ data/DB/dummy_diary.sql
@@ -5,9 +5,9 @@ USE My_Workout;
 
 -- 일기 추가
 INSERT INTO daily_workout 
-VALUES (0, "swm", "발차기 멈추지 말자!", now(), 40, 158, "migon"),
-   	   (0, "swm", "손가락 붙이기!", now(), 42, 159, "migon"),
-	   (0, "yog", "오늘따라 스트레칭이 잘 되었군!", now(), 38, 175, "soomi");
+VALUES (0, "swm", "발차기 멈추지 말자!", curdate(), 40, 158, "migon"),
+   	   (0, "swm", "손가락 붙이기!", curdate(), 42, 159, "migon"),
+	   (0, "yog", "오늘따라 스트레칭이 잘 되었군!", curdate(), 38, 175, "soomi");
 
 SELECT *
 FROM daily_workout;
