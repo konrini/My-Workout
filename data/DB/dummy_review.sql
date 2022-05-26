9fcd0d4455bffb32b296ad07aaf87ca2f4197d76
Migon <79758079+konrini@users.noreply.github.com>
2022-05-26 14:45:56 +0900

    update dummy datas
    sql file

--- data/DB/dummy_review.sql
+++ data/DB/dummy_review.sql
@@ -4,12 +4,16 @@ USE My_Workout;
 -- DELETE FROM review;
 
 -- 운동 영상에 달린 댓글 
-INSERT INTO review 
-VALUES (0, "따라하기 힘드네요", now(), 0, 1, "soomi", "뱅순이", 1),
-       (0, "연습 열심히 하겠습니다.", now(), 0, 5, "migon", "미코미코니", 1),
-	   (0, "vㅔ리 EZ", now(), 0, 4, "gyun", "갓명균", 2);
+-- INSERT INTO review 
+-- VALUES (0, "따라하기 힘드네요", now(), 0, 1, "soomi", "뱅순이", 1),
+--        (0, "연습 열심히 하겠습니다.", now(), 0, 5, "migon", "미코미코니", 1),
+-- 	   (0, "vㅔ리 EZ", now(), 0, 4, "gyun", "갓명균", 2);
 
 SELECT *
 FROM review;
+
+-- SELECT COUNT(*)
+-- FROM review
+-- WHERE video_id = 1;
 
 -- SET sql_safe_updates = 1;
