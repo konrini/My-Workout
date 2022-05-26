b5cd6665e784c83e3a5ec16180b2b51069968053
Migon <79758079+konrini@users.noreply.github.com>
2022-05-27 02:18:09 +0900

    update sql files

--- data/DB/dummy_friend.sql
+++ data/DB/dummy_friend.sql
@@ -6,15 +6,34 @@ USE My_Workout;
 -- 운동 영상에 달린 댓글 
 INSERT INTO friend 
 VALUES (0, "migon", "soomi"),
-	   (0, "soomi", "migon"),
+	   (0, "migon", "sunho"),
+       (0, "soomi", "migon"),
        (0, "soomi", "gyun"),
-       (0, "migon", "gyun");
+       (0, "soomi", "taek"),
+       (0, "sunho", "soomi"),
+       (0, "taek", "gyun"),
+       (0, "taek", "soomi"),
+       (0, "taek", "migon"),
+       (0, "taek", "sunho"),
+       (0, "isthe", "soomi"),
+       (0, "soomi", "isthe"),
+       (0, "isthe", "gyun");
 
 SELECT *
 FROM friend;
 
-SELECT follower_id
-FROM friend
-WHERE target_id = "gyun";
+-- SELECT follower_id
+-- FROM friend
+-- WHERE target_id = "gyun";
 
 -- SET sql_safe_updates = 1;
+
+-- SELECT DISTINCT(follower_id), nickname
+-- FROM user
+-- INNER JOIN friend
+-- ON user.user_id = follower_id;
+
+-- SELECT DISTINCT(nickname)
+-- FROM user
+-- INNER JOIN friend
+-- ON user.user_id = "migon";
